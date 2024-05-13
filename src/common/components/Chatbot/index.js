cea51e09813a7576c33302052dfaf99e357d0a3c
Tony Schroeder <tonyschroeder@MBP-von-Tony.fritz.box>
2024-05-13 20:27:14 +0200

    add chatbot usestate

--- src/common/components/Chatbot/index.js
+++ src/common/components/Chatbot/index.js
@@ -1,22 +1,25 @@
 // import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './chatBot.module.css'
 import axios from 'axios'
 import Button55 from '../Button55'
+import Slide from 'react-reveal/Slide'
 
 export const ChatBot = () => {
   const [input, setInput] = useState('')
   const [messages, setMessages] = useState([
     'Hallo, mein Name ist Twyli. Wie kann ich Ihnen heute helfen?',
   ])
-  const [openChat, setOpenChat] = useState(false)
-  console.log('ChatBot  openChat', openChat)
+  const [openChat, setOpenChat] = useState(true)
+  const [loading, setLoading] = useState(false)
+
 
   const handleInputChange = (e) => {
     setInput(e.target.value)
   }
 
   const handleSubmit = async (e) => {
+    setLoading(true)
     e.preventDefault()
     const userMessage = input
     setInput('')
@@ -30,11 +33,14 @@ export const ChatBot = () => {
     } catch (error) {
       console.error('Error fetching chat response:', error)
     }
+    setLoading(false)
   }
+
+
 
   return (
     <>
-      {openChat ?? (
+      {openChat && (
         <div className={styles.chatBotContainer}>
           <div className={styles.chatBotMessages}>
             {messages.map((msg, index) => {
@@ -42,14 +48,19 @@ export const ChatBot = () => {
               const position = msg.indexOf(':')
 
               return (
-                <div style={{ display: 'flex' }}>
+                <div className={styles.messagesWrapper}
+                  style={{
+                    flexDirection: isUserMsg ? 'row-reverse' : 'row',
+
+                  }}
+                >
                   {/* <IconWrapper></IconWrapper> */}
-                  <img src="logo.png" height={'30px'} width={'30px'} />
+                  <img src={isUserMsg ? "images/User-Avatar.png" : "images/Twyli-avatar.png"} height={'40px'} width={'40px'} />
                   <p
                     style={{
                       textAlign: isUserMsg ? 'right' : 'left',
-                      paddingLeft: isUserMsg ? '40px' : '0px',
-                      paddingRight: isUserMsg ? '0px' : '40px',
+                      paddingLeft: isUserMsg ? '50px' : '0px',
+                      paddingRight: isUserMsg ? '0px' : '50px',
                     }}
                     key={index}
                   >
@@ -62,14 +73,14 @@ export const ChatBot = () => {
 
           <form onSubmit={handleSubmit} className={styles.chatBotInput}>
             <input value={input} onChange={handleInputChange} />
-            <button type="submit">Send</button>
+            <button type="submit">{loading ? '...Denken' : 'Senden'}</button>
           </form>
         </div>
       )}
       <Button55
         className={styles.bubbleBottom}
         onClick={() => {
-          openChat ? setOpenChat(false) : setOpenChat(true)
+          setOpenChat(!openChat);
         }}
       >
         Frag mich
